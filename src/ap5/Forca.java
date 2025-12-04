945b4d197bdef726c73fc4e052ee4b65225e0f4b
Vovk <161193299+vovkhan@users.noreply.github.com>
2025-12-04 20:07:21 -0300

    added method jogar

--- src/ap5/Forca.java
+++ src/ap5/Forca.java
@@ -7,17 +7,20 @@ public class Forca {
     BancoPalavras banco;
     char[] palavra;
     char[] resposta;
+    Scanner scanner;
+    char lastLetraSelecionada;
     int errosMaximo;
     int errosContabilizados;
     ArrayList<Character> letrasEscolhidas;
 
     Forca(){
         this.banco = new BancoPalavras();
-        this.palavra = banco.sortear().toCharArray();
+        this.palavra = this.sortearNovaPalavra;
         this.resposta = "—".repeat(this.palavra.length).toCharArray();
         this.errosMaximo = 6;
         this.errosContabilizados = 0;
         this.letrasEscolhidas = new ArrayList<Character>();
+        this.scanner = new Scanner(System.in);
     }
 
     void exibirErrosContabilizados(){
@@ -38,32 +41,51 @@ public class Forca {
         if(errosContabilizados >= errosMaximo) mensagem = "Você perdeu."; else mensagem = "Você venceu!";
         System.out.printf("%s%n", mensagem);
     }
-    char solicitarLetra(Scanner scanner){
+    char catchLetra(Scanner scanner){
         char letraSelecionada;
+        System.out.printf("Insira um novo valor: ");
         letraSelecionada = scanner.nextLine().replace(" ", "").charAt(0);
         return letraSelecionada;
     }
     boolean validarLetra(char letra){ 
         if(Character.isDigit(letra) || this.letrasEscolhidas.contains(letra)){
-            System.err.printf("A letra '%c' já foi escolhida ou é um valor numérico.", letra);
+            System.err.printf("O valor '%c' já foi escolhido ou é um valor numérico.", letra);
             return false;
         }
         return true;
     }
-    void atualizarErros(char letra){
-        this.letrasEscolhidas.add(letra);
-        this.errosContabilizados++;
-    }
     void atualizarResposta(char letra){
-        for(int i = 0; i < this.palavra.length; i++){
-            if(this.palavra[i] == letra) this.resposta[i] = letra;
-        }
         this.letrasEscolhidas.add(letra);
+
+        for(int i = 0; i < this.palavra.length; i++){
+            if(this.palavra[i] == letra){
+                this.resposta[i] = letra;
+                return;
+            }
+        }
+        this.errosContabilizados++;
+        return;
     }
     boolean respostaIsCorreta(char[] palavra, char[] resposta){
         for(int i = 0; i < palavra.length; i++){
             if(palavra[i] != resposta[i]) return false;
         }
         return true;
+    }
+    char[] sortearNovaPalavra(){
+        return this.banco.sortear().toCharArray();
+    }
+    void 
+
+    void jogar(boolean sortearNovaPalavra){
+        if(sortearNovaPalavra) this.palavra = this.sortearNovaPalavra;
+
+        while(true){
+            this.exibirResposta();
+            this.lastLetraSelecionada = this.catchLetra(this.scanner);
+            if(this.validarLetra(this.lastLetraSelecionada)){
+                this.atualizarResposta();
+            }
+        }
     }
 }
