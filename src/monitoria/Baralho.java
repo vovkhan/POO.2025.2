5f693820b962ce2d5fd2517c0b4ebe9ce922cd33
vovkhan <vivaldera@gmail.com>
2025-12-13 16:42:20 -0300

    monitoria done

--- src/monitoria/Baralho.java
+++ src/monitoria/Baralho.java
@@ -1,11 +1,13 @@
 package monitoria;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Baralho {
-    ArrayList<Carta> cartas;
     static String[] nomes = new String[]{"Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei", };
     static String[] naipes = new String[]{"Copas", "Espadas", "Ouros", "Paus"};
+    ArrayList<Carta> cartas;
+    Random random;
 
     Baralho(){
         cartas = new ArrayList<Carta>();
@@ -17,6 +19,40 @@ public class Baralho {
         for(int k = 0; k < 4; k++){
             cartas.add(new Carta("Coringa", "Coringa"));
         }
+        random = new Random();
     }
-    
+
+    void embaralhar(){
+        int index = 0;
+        int indexLimite = (naipes.length * nomes.length) + 4;
+        for(int i = 0; i < naipes.length; i++){
+            for(int j = 0; j < nomes.length; j++){
+                index = random.nextInt(indexLimite);
+                cartas.add(cartas.get(index));
+                cartas.remove(index);
+            }
+        }
+    }
+
+    Carta darCarta(){
+        if(this.temCarta()){
+            Carta cartaRetornada = cartas.getLast();
+            cartas.removeLast();
+            return cartaRetornada;
+        }
+        return null;
+    }
+
+    boolean temCarta(){
+        if(cartas.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    void imprimirBaralho(){
+        for(Carta c: cartas){
+            c.imprimir();
+        }
+    }
 }
