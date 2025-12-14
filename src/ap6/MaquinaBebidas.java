8b72fd30b59498b5d34f0c5b2842dce2b429ad34
vovkhan <vivaldera@gmail.com>
2025-12-13 23:45:01 -0300

    ap6 funcional?

--- src/ap6/MaquinaBebidas.java
+++ src/ap6/MaquinaBebidas.java
@@ -7,7 +7,6 @@ public class MaquinaBebidas {
     double[] moedasValidas;
     Bebida[] bebidas;
     ArrayList<Double> moedasInseridas;
-    int bebidaEscolhidaIndice;
     Scanner scanner;
 
     MaquinaBebidas(){
@@ -24,53 +23,52 @@ public class MaquinaBebidas {
     void inserirMoedas(){
         while(true){
             double moeda;
-            System.out.printf("Insira um valor de moeda ou '0' para continuar: ");
+            System.out.printf("Insira um valor de moeda em centavos ou '0' para continuar: ");
             moeda = scanner.nextDouble();
             if(moeda <= 0) break;
             for(double m: moedasValidas){
-                if(moeda == m){
-                    moedasInseridas.add(moeda);
+                if(moeda/100 == m){
+                    moedasInseridas.add(moeda/100);
                 }
             }
         }
     }
     double contarMoedas(ArrayList<Double> moedasArrayList){
         double soma = 0;
-        for(double m: moedasArrayList) soma += m;
+        if(!moedasArrayList.isEmpty()){
+            for(double m: moedasArrayList) soma += m;
+        }
         return soma;
     };
-    void escolherBebida(){
+    int escolherBebida(){
         int escolha = 0;
         System.out.println("Bebidas disponíveis: ");
         for(int i = 0; i < bebidas.length; i++){
-            System.out.printf("%d. R$%.2f - %s", (i+1), bebidas[i].valor, bebidas[i].nome);
+            System.out.printf("%d. R$%.2f - %s.%n", (i+1), bebidas[i].valor, bebidas[i].nome);
         }
-        while(escolha < 1 && escolha > bebidas.length){
-            System.out.print("Insira o id da escolha ou '99' para estornar suas moedas: ");
+        while(!(escolha >= 1 && escolha <= bebidas.length)){
+            System.out.print("Insira o id da escolha ou '0' para estornar suas moedas: ");
             escolha = scanner.nextInt();
-            if(escolha == 99){
-                estornarMoedas();
-                break;
-            }
+            if(escolha == 0) return -1;
         }
-        this.bebidaEscolhidaIndice = (escolha-1);
+        return (escolha-1);
     }
-    boolean saldoSuficiente(){
-        if(this.contarMoedas(moedasInseridas) < bebidas[bebidaEscolhidaIndice].valor) return false;
+    boolean saldoSuficiente(int index){
+        if(this.contarMoedas(moedasInseridas) < bebidas[index].valor) return false;
         return true;
     }
     void estornarMoedas(){
-        for(int i = 0; i < moedasInseridas.size(); i++){
-            System.out.printf("Moeda estornada: %.2f.%n", moedasInseridas.get(i));
+        while(!moedasInseridas.isEmpty()){
+            System.out.printf("Moeda estornada: %.2f.%n", moedasInseridas.getFirst());
             moedasInseridas.removeFirst();
         }
     }
-    void descontarMoedas(double valor){
-        ArrayList<Double> moedasDescontadas = moedasInseridas;
+    ArrayList<Double> descontarMoedas(double valor){
+        ArrayList<Double> moedasDescontadas = new ArrayList<Double>(moedasInseridas);
         int soma = 0;
         for(int i = 0; i < moedasInseridas.size(); i++){
             if(soma == valor) break;
-            for(int j = moedasInseridas.size(); j > 0; j--){
+            for(int j = moedasValidas.length-1; j > 0; j--){
                 if(moedasInseridas.get(i) == moedasValidas[j]){
                     if(soma != valor){
                         soma += moedasInseridas.get(i);
@@ -79,12 +77,49 @@ public class MaquinaBebidas {
                 }
             }
         }
-        System.out.printf("Valor inserido: %.2f.%n", contarMoedas(moedasInseridas));
-        System.out.printf("Valor após desconto: %.2f.%n", contarMoedas(moedasDescontadas));
+        return moedasDescontadas;
     }
     void prepararBebida(){
-        if(saldoSuficiente()){
-            return;
+        int escolha = this.escolherBebida();
+        if(escolha < 0) return;
+        if(saldoSuficiente(escolha)){
+            System.out.printf("%s preparado, bom proveito.%n", bebidas[escolha].nome);
+            moedasInseridas = this.descontarMoedas(bebidas[escolha].valor);
+            System.out.printf("Valor restante: %.2f.%n", contarMoedas(moedasInseridas));
+        } else {
+            System.out.printf("Valor insuficiente para '%s', insira mais moedas ou escolha outra bebida.%n", bebidas[escolha].nome);
+        }
+    }
+    void iniciar(){
+        while (true) {
+            int acao = 0;
+            System.out.println("Menu de ações:");
+            System.out.printf("""
+                    1. Inserir moedas.
+                    2. Selecionar e preparar bebida.
+                    3. Estornar moedas.
+                    0. Encerrar interação.
+                    Saldo disponível: %.2f
+                    """, this.contarMoedas(moedasInseridas));
+            System.out.print("Ação: ");
+            acao = scanner.nextInt();
+
+            switch (acao) {
+                case 1:
+                    this.inserirMoedas();    
+                    break;
+                case 2:
+                    this.prepararBebida();
+                    break;
+                case 3:
+                    this.estornarMoedas();
+                    break;
+                case 0:
+                    this.estornarMoedas();
+                    return;                
+                default:
+                    break;
+            }
         }
     }
 }
---
+++ src/ap6/MaquinaBebidasTeste.java
@@ -0,0 +1,9 @@
+package ap6;
+
+public class MaquinaBebidasTeste {
+    public static void main(String[] args){
+        MaquinaBebidas maquina = new MaquinaBebidas();
+        maquina.iniciar();
+
+    }
+}
