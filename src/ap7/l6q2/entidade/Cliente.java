d33384b87a1ad2d80f603154dda9b62af652e798
vovkhan <vivaldera@gmail.com>
2025-12-17 18:21:58 -0300

    simple cliente

--- src/ap7/l6q2/entidade/Cliente.java
+++ src/ap7/l6q2/entidade/Cliente.java
@@ -1,5 +1,37 @@
 package l6q2.entidade;
 
 public class Cliente {
-    
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private Endereco endereco;
+
+    Cliente(String nome, String cpf, String telefone, Endereco endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public String getCpf() {
+        return this.cpf;
+    }
+    public String getTelefone() {
+        return this.telefone;
+    }
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
--- src/ap7/l6q2/repositorio/RepositorioContasVetor.java
+++ src/ap7/l6q2/repositorio/RepositorioContasVetor.java
@@ -1,5 +1,8 @@
 package l6q2.repositorio;
 
+import l6q2.entidade.Conta;
+import java.util.ArrayList;
+
 public class RepositorioContasVetor {
-    
+    ArrayList<Conta> contas;
 }
